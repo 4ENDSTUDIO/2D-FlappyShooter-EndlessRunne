33e3d30de1a9729df4cc9df531343296c93ce342
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-07-29 20:41:29 +0700

    push healthpoint

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,8 +8,24 @@ public class Player : MonoBehaviour
     public float velocity = 1;
     private Rigidbody2D rb;
     public GameObject Smoke;
+    public GameObject heart1, heart2, heart3;
+    public int playerHealth = 3;
+    int playerLayer, enemyLayer;
+    bool coroutineAllowed = true;
+    Color color;
+    Renderer rend;
     void Start()
     {
+        playerLayer = this.gameObject.layer;
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
+        rend = GetComponent<Renderer>();
+        color = rend.material.color;
+
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -27,7 +43,47 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        gameManager.GameOver(); 
+        if(collision.gameObject.tag.Equals("Enemy"))
+        {
+            playerHealth -= 1;
+            switch (playerHealth)
+            {
+                case 2:
+                    heart3.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+                case 1:
+                    heart2.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+                case 0:
+                    heart1.gameObject.SetActive(false);
+                    if (coroutineAllowed)
+                        StartCoroutine("Immortal");
+                    break;
+            }
+            if(playerHealth < 1)
+            {
+                gameManager.GameOver();
+            }
+           
+        }
+         
+    }
+
+    IEnumerator Immortal()
+    {
+        coroutineAllowed = false;
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, true);
+        color.a = 0.5f;
+        rend.material.color = color;
+        yield return new WaitForSeconds(3f);
+        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
+        color.a = 1f;
+        rend.material.color = color;
+        coroutineAllowed = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
