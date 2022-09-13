11eac985e187ef52a7ebbc23884d3d3e85e721ec
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-09-13 17:36:37 +0700

    push die bos shake, and partikel die

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
-    
+    public Animator anim;
    public int currentHealth = 100;
     public Slider healthBar;
+    public ShakeCamera shake;
+    public GameObject blood;
 
-    
+
     void Start()
     {
         
@@ -18,6 +20,8 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
+
         healthBar.value = currentHealth;
     }
 
@@ -26,9 +30,22 @@ public class Boss : MonoBehaviour
         currentHealth -= damagee;
         if(currentHealth <= 0)
         {
+            shake.CameraShake();
             Score.BossScore();
             Destroy(gameObject);
             healthBar.value = 0;
+            Instantiate(blood, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Bullet"))
+        {
+            if(currentHealth >=1)
+            {
+                anim.SetTrigger("Hit");
+            }
         }
     }
 }
